9bb1e5e19c0e1be6f07c05b1463b5f5a8d308443
jveres <janos.veres@gmail.com>
2022-04-02 19:25:41 +0200

    updates

--- utils/start_serf.sh
+++ utils/start_serf.sh
@@ -2,12 +2,12 @@
 set -e
 
 exec serf agent \
-  -node="${FLY_REGION}-$(hostname)" \
+  -node="$FLY_REGION-$(hostname)" \
   -profile=wan \
   -join $FLY_APP_NAME.internal \
   -bind="fly-local-6pn:7946" \
   -rpc-addr="fly-local-6pn:7373" \
-  -tag region="${FLY_REGION}" \
-  -tag app="keydb" \
+  -tag region="$FLY_REGION" \
+  -tag app="$FLY_APP_NAME" \
   -event-handler "member-join=member_join.sh" \
   -event-handler "member-leave,member-failed=member_leave.sh"
