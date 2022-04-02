7010f7732f9184ef0c5e12d3741d3f0eaf922233
jveres <janos.veres@gmail.com>
2022-04-02 19:20:51 +0200

    updates

--- utils/start_serf.sh
+++ utils/start_serf.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 set -e
 
-JOIN_ADDR=$(dig aaaa $FLY_APP_NAME.internal +short | tail -1)
-LOCAL_IP=$(grep fly-local-6pn /etc/hosts | awk '{print $1}')
-
 exec serf agent \
   -node="${FLY_REGION}-$(hostname)" \
   -profile=wan \
-  -join="[$JOIN_ADDR]" \
-  -bind="[$LOCAL_IP]:7946" \
-  -rpc-addr="[$LOCAL_IP]:7373" \
+  -join $FLY_APP_NAME.internal \
+  -bind="fly-local-6pn:7946" \
+  -rpc-addr="fly-local-6pn:7373" \
   -tag region="${FLY_REGION}" \
   -tag app="keydb" \
   -event-handler "member-join=member_join.sh" \
